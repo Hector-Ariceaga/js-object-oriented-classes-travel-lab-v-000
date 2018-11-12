a3fbc01764fcd7c22185803c32354ee8f4e3d342
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-11-12 20:05:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-
+class Driver {
+  constructor(name, date) {
+    this.name = name
+    this.date = date
+  }
+  
+  startDate() {
+    const startDate = new Date(this.date)
+  }
+  
+  yearsExperienceFromBeginningOf(year) {
+    
+  }
+}
