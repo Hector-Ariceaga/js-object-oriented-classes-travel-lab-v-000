a322aa2d7c99a63cf0dee5cb72e7a5cca3adcc9b
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-11-12 20:40:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,8 +22,7 @@ class Route {
     console.log(this.endingLocation)
     let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
     
-    let startingAvenue = eastWest.filter(avenue => avenue === this.beginningLocation.horizontal)
-    console.log(eastWest.indexOf(startingAvenue))
+    let startingAvenue = eastWest.indexOf(this.beginningLocation.horizontal)
     
     let horizontalTravel = this.endingLocation.horizontal - this.endingLocation.horizontal
   }
