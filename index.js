84396b4bf0b652e5172899dcbd0cce6eaa0107bf
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-11-12 21:09:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,9 +15,6 @@ class Route {
     this.endingLocation = endingLocation
   }
   
-  blocksTravelled() {
-    let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
-    
     let startingAvenue = eastWest.indexOf(this.beginningLocation.horizontal)
     let endingAvenue = eastWest.indexOf(this.endingLocation.horizontal)
     let horizontalTravel = endingAvenue - startingAvenue
